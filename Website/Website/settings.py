e305797d67209f9525bfc2293ea7d83744a59e65
Bilaljuly07 <91802908+Bilaljuly07@users.noreply.github.com>
2023-01-24 01:56:47 +0530

    hello3

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+t7+2^n06gl+(oy%!ljdvng=g!2kl!+7s5vda50pfdj*&2633'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
