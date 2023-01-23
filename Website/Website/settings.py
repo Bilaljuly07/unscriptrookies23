c6bea035ea2f2b74763ecd86191b875f5debf1eb
Bilaljuly07 <91802908+Bilaljuly07@users.noreply.github.com>
2023-01-23 20:54:56 +0530

    hello1

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+t7+2^n06gl+(oy%!ljdvng=g!2kl!+7s5vda50pfdj*&2633'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
